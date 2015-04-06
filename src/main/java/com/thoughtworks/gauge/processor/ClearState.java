f4e94cca4defc38f446baaa89a2174f3e6ba0e17
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-04-06 15:11:49 +0530

    Changing flag name to gauge_clear_state_level

--- src/main/java/com/thoughtworks/gauge/processor/ClearState.java
+++ src/main/java/com/thoughtworks/gauge/processor/ClearState.java
@@ -3,14 +3,14 @@ package com.thoughtworks.gauge.processor;
 import com.thoughtworks.gauge.ClassInstanceManager;
 
 public enum ClearState {
-    SUITE_LEVEL("suite_level"), SPEC_LEVEL("spec_level"), SCENARIO_LEVEL("scenario_level");
+    SUITE_LEVEL("suite"), SPEC_LEVEL("spec"), SCENARIO_LEVEL("scenario");
     private final String level;
 
     ClearState(String level) {
         this.level = level;
     }
 
-    public static final String clear_state_flag = "clear_state";
+    public static final String clear_state_flag = "gauge_clear_state_level";
 
     public static void clear(ClearState currentPosition){
         String level = System.getenv(clear_state_flag);
