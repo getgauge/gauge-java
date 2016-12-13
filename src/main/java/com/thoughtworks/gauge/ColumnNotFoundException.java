26355e94d9774b59d44bc637ac6ded116f787d28
Sheikh, Haroon <haroon.sheikh@sky.uk>
2016-12-13 23:23:05 +0000

    amends to throw an IndexOutOfBoundsException instead of a custom

--- src/main/java/com/thoughtworks/gauge/ColumnNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.gauge;
-
-public class ColumnNotFoundException extends RuntimeException {
-    public ColumnNotFoundException(final String error) {
-        super(error);
-    }
-}
