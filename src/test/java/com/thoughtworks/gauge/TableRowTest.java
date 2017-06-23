18405e834ece1d3e746ca9859fb34a6261987c90
Rabih Hallage <rabih.hallage@cardinalhealth.com>
2017-06-23 16:12:45 +0300

    Override equals in Guage Table, TableRow, and TableCells #91

--- src/test/java/com/thoughtworks/gauge/TableRowTest.java
+++ src/test/java/com/thoughtworks/gauge/TableRowTest.java
@@ -1,19 +1,55 @@
 package com.thoughtworks.gauge;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-
 
 public class TableRowTest {
 
     @Test
-    public void testToString(){
+    public void testToString() {
         TableRow row = new TableRow();
         row.addCell("hello", "world");
         row.addCell("foo", "bar");
-        
-        assertEquals("TableRow{cells={hello=world, foo=bar}}",row.toString());
+
+        assertEquals("TableRow{cells={hello=world, foo=bar}}", row.toString());
+    }
+
+    @Test
+    public void testEqualSameValuesAndOrder() {
+        TableRow row = new TableRow();
+        row.addCell("hello", "world");
+        row.addCell("foo", "bar");
+
+        TableRow otherRow = new TableRow();
+        otherRow.addCell("hello", "world");
+        otherRow.addCell("foo", "bar");
+        assertTrue(row.equals(otherRow));
+    }
+
+    @Test
+    public void testEqualSameValuesDifferentOrder() {
+        TableRow row = new TableRow();
+        row.addCell("hello", "world");
+        row.addCell("foo", "bar");
+
+        TableRow otherRow = new TableRow();
+        otherRow.addCell("foo", "bar");
+        otherRow.addCell("hello", "world");
+        assertTrue(row.equals(otherRow));
+    }
+
+    @Test
+    public void testEqualDifferentValues() {
+        TableRow row = new TableRow();
+        row.addCell("hello", "world");
+        row.addCell("foo", "bar");
+
+        TableRow otherRow = new TableRow();
+        otherRow.addCell("foo1", "bar1");
+        otherRow.addCell("hello1", "world1");
+        assertFalse(row.equals(otherRow));
     }
 }
