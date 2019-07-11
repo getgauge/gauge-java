f88da1f4bb9d286217baad31114a6449be4b4238
Nehashri <neha.nehe@gmail.com>
2019-07-11 16:33:14 +0530

    adding copyright to files

--- src/main/java/com/thoughtworks/gauge/connection/MessageDispatcher.java
+++ src/main/java/com/thoughtworks/gauge/connection/MessageDispatcher.java
@@ -1,4 +1,4 @@
-// Copyright 2015 ThoughtWorks, Inc.
+// Copyright 2019 ThoughtWorks, Inc.
 
 // This file is part of Gauge-Java.
 
@@ -12,6 +12,7 @@
 //
 // You can redistribute it and/or modify it under the terms of either license.
 // We would then provide copied of each license in a separate .txt file with the name of the license as the title of the file.
+
 package com.thoughtworks.gauge.connection;
 
 
