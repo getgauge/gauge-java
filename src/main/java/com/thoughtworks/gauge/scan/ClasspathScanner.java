846e58f93a33f2ab164501258244406ae2832a1f
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2020-03-06 14:09:55 +0530

    Upgrade checkstyle and reflection libraries
    * Upgrade checkstyle library version
    * Upgrade reflection library version
    * Ignore crlf and lf check because of compatibilty across
     windows and unix
    * Modify the to use new api for testing reflection

--- src/main/java/com/thoughtworks/gauge/scan/ClasspathScanner.java
+++ src/main/java/com/thoughtworks/gauge/scan/ClasspathScanner.java
@@ -71,12 +71,12 @@ public class ClasspathScanner {
     private boolean shouldScan(String s) {
         final String packagesToScan = System.getenv(PACKAGE_TO_SCAN);
         if (packagesToScan == null || packagesToScan.isEmpty()) {
-            return new FilterBuilder().include(".+\\.class").apply(s);
+            return new FilterBuilder().include(".+\\.class").test(s);
         }
         final String[] packages = packagesToScan.split(",");
         for (String packageToScan : packages) {
             String regex = String.format(".?\\.??%s\\..+\\.class", packageToScan);
-            if (new FilterBuilder().include(regex).apply(s)) {
+            if (new FilterBuilder().include(regex).test(s)) {
                 return true;
             }
         }
