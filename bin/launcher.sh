87fc1a510eaf50c3b2ed26e919a16d9179f3fa63
sriv <srikanth.ddit@gmail.com>
2020-04-13 18:14:25 +0530

    fix launcher.sh pom path
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -38,7 +38,7 @@ if [ -z "${gauge_custom_classpath}" ]; then
     GAUGE_GRADLE_BUILD_FILE="${GAUGE_GRADLE_BUILD_FILE:-build.gradle}"
     if test -f $GAUGE_POM_XML; then
         cp_tmp_file="$TMPDIR$RANDOM-$RANDOM-cp.txt"
-        mvn -q -f GAUGE_MAVEN_POM_FILE -DincludeScope=compile dependency:build-classpath -Dmdep.outputFile=$cp_tmp_file
+        mvn -q -f $GAUGE_MAVEN_POM_FILE -DincludeScope=compile dependency:build-classpath -Dmdep.outputFile=$cp_tmp_file
         class_path=$(cat $cp_tmp_file)
     fi
     if test -f $GAUGE_GRADLE_BUILD_FILE; then
