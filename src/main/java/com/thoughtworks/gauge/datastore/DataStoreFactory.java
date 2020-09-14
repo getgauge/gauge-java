f0c3b8a4084a8c8aa631aab1bb96775b521aeef5
Vinay Shankar Shukla <vinaysh@thoughtworks.com>
2020-09-14 16:34:34 +0530

    Deprecate datastore factory. Add specific data stores. (#485)
    * Deprecate datastore factory. Add specific data stores.
    
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>
    
    * Clone FTs from branch.
    
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>
    
    * Address review comments.
    
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>
    
    * Check for null values. Fix javadoc
    
    Signed-off-by: BugDiver <vinaysh@thoughtworks.com>
    
    Co-authored-by: Zabil Cheriya Maliackal <zabil@users.noreply.github.com>

--- src/main/java/com/thoughtworks/gauge/datastore/DataStoreFactory.java
+++ src/main/java/com/thoughtworks/gauge/datastore/DataStoreFactory.java
@@ -5,6 +5,17 @@
  *----------------------------------------------------------------*/
 package com.thoughtworks.gauge.datastore;
 
+import static com.thoughtworks.gauge.GaugeConstant.ENABLE_MULTITHREADING_ENV;
+
+
+/**
+ * @deprecated DataStoreFactory is no longer valid. This API will throw an Exception in multithreaded execution.
+ * <p>Use specific data stores instead.</p>
+ * @see com.thoughtworks.gauge.datastore.SuiteDataStore
+ * @see com.thoughtworks.gauge.datastore.SpecDataStore
+ * @see com.thoughtworks.gauge.datastore.ScenarioDataStore
+ */
+@Deprecated
 public class DataStoreFactory {
     private static ThreadLocal<DataStore> suiteDataStore = new InheritableThreadLocal<DataStore>() {
         @Override
@@ -30,6 +41,9 @@ public class DataStoreFactory {
      * @return The current instance of the SuiteDataStore
      */
     public static DataStore getSuiteDataStore() {
+        if (isMultithreadingExecution()) {
+            throw new RuntimeException("DataStoreFactory cannot be used for multithreaded execution. Use SuiteDataStore.");
+        }
         return suiteDataStore.get();
     }
 
@@ -37,14 +51,37 @@ public class DataStoreFactory {
      * @return The current instance of the SpecDataStore
      */
     public static DataStore getSpecDataStore() {
+        if (isMultithreadingExecution()) {
+            throw new RuntimeException("DataStoreFactory cannot be used for multithreaded execution. Use SpecDataStore.");
+        }
         return specDataStore.get();
     }
+
 
     /**
      * @return The current instance of the ScenarioDataStore
      */
     public static DataStore getScenarioDataStore() {
+        if (isMultithreadingExecution()) {
+            throw new RuntimeException("DataStoreFactory cannot be used for multithreaded execution. Use ScenarioDataStore.");
+        }
         return scenarioDataStore.get();
+    }
+
+    static void clearSuiteDataStore() {
+        suiteDataStore.get().clear();
+    }
+
+    static void clearSpecDataStore() {
+        specDataStore.get().clear();
+    }
+
+    static void clearScenarioDataStore() {
+        scenarioDataStore.get().clear();
+    }
+
+    private static boolean isMultithreadingExecution() {
+        return Boolean.valueOf(System.getenv(ENABLE_MULTITHREADING_ENV));
     }
 
 }
---
+++ src/main/java/com/thoughtworks/gauge/datastore/SuiteDataStore.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------
+ *  Copyright (c) ThoughtWorks, Inc.
+ *  Licensed under the Apache License, Version 2.0
+ *  See LICENSE.txt in the project root for license information.
+ *----------------------------------------------------------------*/
+
+package com.thoughtworks.gauge.datastore;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SuiteDataStore {
+    private static ThreadLocal<ConcurrentHashMap<Object, Object>> map = ThreadLocal.withInitial(ConcurrentHashMap::new);
+
+    /**
+     * @param key   - Key of the data entry
+     * @param value - value of the Data entry
+     */
+    public static synchronized void put(Object key, Object value) {
+        if (key != null && value != null)  {
+            map.get().put(key, value);
+        }
+    }
+
+    /**
+     * @param key - Key of the data entry to remove
+     * @return The value of the entry removed. Null if no entry.
+     */
+    public static synchronized Object remove(Object key) {
+        if (key != null) {
+            return map.get().remove(key);
+        }
+        return null;
+    }
+
+    /**
+     * @param key - Key of the data entry whose value is needed
+     * @return The value corresponding to the key. null if there is no value stored
+     */
+    public static synchronized Object get(Object key) {
+        if (key != null) {
+            return map.get().get(key);
+        }
+        return null;
+    }
+
+    static synchronized void clear() {
+        map.get().clear();
+    }
+}
