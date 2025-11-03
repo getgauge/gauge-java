1ae5c396391d4306644233bc1fc6300a11425502
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-11-03 22:49:39 +0800

    Reduce a dependency by replacing org.json with json, already present for grpc
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- src/main/java/com/thoughtworks/gauge/Logger.java
+++ src/main/java/com/thoughtworks/gauge/Logger.java
@@ -5,8 +5,11 @@
  *----------------------------------------------------------------*/
 package com.thoughtworks.gauge;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import org.assertj.core.util.Throwables;
-import org.json.JSONObject;
 
 public class Logger {
     public static void info(String message) {
@@ -40,21 +43,26 @@ public class Logger {
 
     public static void fatal(String message, Throwable t) {
         fatal(String.format("%s\n%s\n%s", message, t.getMessage(), Throwables.getStackTrace(t)));
-
     }
 
     private static void logToStdout(String level, String message) {
-        System.out.println(getJsonObject(level, message));
-    }
-
-    private static JSONObject getJsonObject(String level, String message) {
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("logLevel", level);
-        jsonObj.put("message", message);
-        return jsonObj;
+        System.out.println(LogMessage.of(level, message));
     }
 
     private static void logToStdErr(String level, String message) {
-        System.err.println(getJsonObject(level, message));
+        System.err.println(LogMessage.of(level, message));
+    }
+
+    private record LogMessage(@Expose @SerializedName("logLevel") String level, @Expose @SerializedName("message") String message) {
+        private static final Gson GSON = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+        public static LogMessage of(String level, String message) {
+            return new LogMessage(level, message);
+        }
+
+        @Override
+        public String toString() {
+            return GSON.toJson(this);
+        }
     }
 }
